aa33fd74416f82daf38e98794ebeabfb68ccd906
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-09 09:23:41 -0700

    Update Book.java

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,51 +1,55 @@
 package model;
 
-import java.math.BigDecimal;
-
 public class Book {
     private int bookId;
     private String title;
     private int authorId;
     private int categoryId;
-    private BigDecimal price;
-    private int quantity;
+    private double price;
+    private int stock;
     private String description;
 
-    public Book() {}
+    
+    public Book(String title, int authorId, int categoryId, double price, int stock) {
+        this.title = title;
+        this.authorId = authorId;
+        this.categoryId = categoryId;
+        this.price = price;
+        this.stock = stock;
+        this.description = "Basic Book Description"; 
+    }
 
-  
+    
+    public int getBookId() {
+        return bookId;
+    }
 
-    public int getBookId() { return bookId; }
-    public void setBookId(int bookId) { this.bookId = bookId; }
+    public String getTitle() {
+        return title;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public int getAuthorId() {
+        return authorId;
+    }
 
-    public int getAuthorId() { return authorId; }
-    public void setAuthorId(int authorId) { this.authorId = authorId; }
+    public int getCategoryId() {
+        return categoryId;
+    }
 
-    public int getCategoryId() { return categoryId; }
-    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
+    public double getPrice() {
+        return price;
+    }
 
-    public BigDecimal getPrice() { return price; }
-    public void setPrice(BigDecimal price) { this.price = price; }
+    public int getStock() {
+        return stock;
+    }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public String getDescription() {
+        return description;
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-            "bookId=" + bookId +
-            ", title='" + title + '\'' +
-            ", authorId=" + authorId +
-            ", categoryId=" + categoryId +
-            ", price=" + price +
-            ", quantity=" + quantity +
-            ", description='" + description + '\'' +
-            '}';
+    
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
     }
 }
