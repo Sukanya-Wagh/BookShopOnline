92031d60d57917254de92b3978036ac4ae021000
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-09 07:36:23 -0700

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,47 +2,42 @@ package main;
 
 import dao.DBConnection;
 import dao.UserDAO;
+import dao.BookDAO;
 import model.User;
+import model.Book;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
-         
+            System.out.println(" Welcome to Online BookShop Console Application!");
             Connection con = DBConnection.getConnection();
-            System.out.println("Connected to DB!");
+            System.out.println(" Connected to DB successfully!\n");
 
-          
             UserDAO userDAO = new UserDAO(con);
-)
-            User user1 = new User("john_doe", "john@example.com", "password123", "1234567890", "123 Street, City", "customer");
-            userDAO.insertUser(user1);
-            System.out.println("User inserted!");
+            User user = new User("sukanya_wagh", "waghsukanya153@gmail.com", "pass123", "9876543210", "Pune", "customer");
+            userDAO.insertUser(user);
+            System.out.println(" User inserted: " + user.getUsername());
 
-            System.out.println("Users in DB:");
-            for (User u : userDAO.getAllUsers()) {
-                System.out.println(u);
+            System.out.println("\n Users in the database:");
+            List<User> users = userDAO.getAllUsers();
+            for (User u : users) {
+                System.out.println(u.getUserId() + " | " + u.getUsername() + " | " + u.getEmail());
             }
 
-            user1.setUsername("john_doe_updated");
-            user1.setPassword("newpassword123");
-            userDAO.updateUser(user1);
-            System.out.println("User updated!");
+           
+            BookDAO bookDAO = new BookDAO(con);
+            Book book = new Book("Java Basics", 1, 1, 499.99, 10); 
+            bookDAO.insertBook(book);
+            System.out.println("\n Book inserted: " + book.getTitle());
 
-            
-            System.out.println("Users in DB after update:");
-            for (User u : userDAO.getAllUsers()) {
-                System.out.println(u);
-            }
-
-            userDAO.deleteUser(user1.getUserId());
-            System.out.println("User deleted!");
-
-
-            System.out.println("Users in DB after deletion:");
-            for (User u : userDAO.getAllUsers()) {
-                System.out.println(u);
+            System.out.println("\n Books in the database:");
+            List<Book> books = bookDAO.getAllBooks();
+            for (Book b : books) {
+       
+                System.out.println(b.getBookId() + " | " + b.getTitle() + " | ₹" + b.getPrice());
             }
 
         } catch (Exception e) {
