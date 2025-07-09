d3af537573ef47a87cec02fbc711e6197f48e195
Sukanya-Wagh <waghsukanya153@gmail.com>
2025-07-09 09:24:38 -0700

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,31 +12,30 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println(" Welcome to Online BookShop Console Application!");
+            System.out.println("Welcome to Online BookShop Console Application!");
             Connection con = DBConnection.getConnection();
-            System.out.println(" Connected to DB successfully!\n");
+            System.out.println("Connected to DB successfully!\n");
 
+            
             UserDAO userDAO = new UserDAO(con);
-            User user = new User("sukanya_wagh", "waghsukanya153@gmail.com", "pass123", "9876543210", "Pune", "customer");
+            User user = new User("gauri_wagh", "gauri@example.com", "gauri123", "9876500011", "Mumbai", "customer");
             userDAO.insertUser(user);
-            System.out.println(" User inserted: " + user.getUsername());
+            System.out.println("User inserted: " + user.getUsername());
 
-            System.out.println("\n Users in the database:");
+            
+            System.out.println("\nUsers in the database:");
             List<User> users = userDAO.getAllUsers();
             for (User u : users) {
                 System.out.println(u.getUserId() + " | " + u.getUsername() + " | " + u.getEmail());
             }
 
-           
-            BookDAO bookDAO = new BookDAO(con);
-            Book book = new Book("Java Basics", 1, 1, 499.99, 10); 
-            bookDAO.insertBook(book);
-            System.out.println("\n Book inserted: " + book.getTitle());
 
-            System.out.println("\n Books in the database:");
+            BookDAO bookDAO = new BookDAO(con);
+
+            
+            System.out.println("\nBooks in the database:");
             List<Book> books = bookDAO.getAllBooks();
             for (Book b : books) {
-       
                 System.out.println(b.getBookId() + " | " + b.getTitle() + " | ₹" + b.getPrice());
             }
 
